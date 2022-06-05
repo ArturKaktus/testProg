75e29e5f3714d2561cdb88ef0f6517cbd872dbc4
Milovidov Artur <aomilovidov@gmail.com>
2022-06-05 23:02:32 +0500

    add CSharp result

---
+++ c_sharp/TestOil/TestOil/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOil
+{
+    public interface ITreeNode
+    {
+        int TypeObj { get; set; }
+        string NameObj { get; set; }
+        List<ITreeNode> Childs { get; set; }
+        bool hasThisChild(int typeobj, string nameobj);
+    }
+
+    public class Oil : ITreeNode
+    {
+        public int TypeObj { get; set; }
+        public string NameObj { get; set; }
+        public List<ITreeNode> Childs { get; set; }
+        public bool hasThisChild(int typeobj, string nameobj)
+        {
+            return (Childs.Exists(x => x.TypeObj == typeobj && x.NameObj == nameobj) ? true : false);            
+        }
+
+        public Oil(int typeobj, string nameobj, List<ITreeNode> child)
+        {
+            TypeObj = typeobj;
+            NameObj = nameobj;
+            Childs = child;
+        }
+    }
+    internal class Program
+    {      
+        
+        static void Main(string[] args)
+        {
+            
+        }   
+    }
+}
